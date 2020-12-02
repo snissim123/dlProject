f322bbc6497f516a1f6876a045661976a42a8d06
Sandra Nissim <sandranissim@Sandras-MBP.hsd1.ca.comcast.net>
2020-12-01 19:38:27 -0800

    finished prelim website

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 
 const headerStyle = {
-  backgroundColor: "#416975",
+  backgroundColor: "#3B6275",
   padding: 40
 };
 
 const titleStyle = {
   color: "#ADCEDE",
-  fontSize: "32px",
+  fontSize: "36px",
 }
 
 const subStyle = {
@@ -30,8 +30,25 @@ const rightStyle = {
 }
 
 const courseStyle = {
-  padding: 100,
+  paddingTop: 100,
 }
+
+const aboutStyle = {
+  color: "#61A2C2",
+  float: "left",
+  fontSize: "32px",
+  paddingLeft: "5%",
+}
+
+const contentStyle = {
+  color: "#1B2C36",
+  float: "left",
+  fontSize: "18px",
+  width: "90%",
+  textAlign: "left",
+  paddingLeft: "5%",
+}
+
 
 function App() {
   return (
@@ -39,6 +56,7 @@ function App() {
       <header style={headerStyle}>
           <p style={titleStyle}>Analyzing the Classification of Skin Lesions Through Convolutional Neural Networks</p>
       </header>
+
       <body style={subStyle}>
         <div style={leftStyle}>
           <p><b>Jordan Zax</b></p>
@@ -54,6 +72,41 @@ function App() {
           <p>Northwestern University</p>
         </div>
       </body>
+
+      <div>
+        <div style={aboutStyle}>
+          <p><em><b>About</b></em></p>
+        </div>
+        <div style={contentStyle}>
+          <p>
+            We have made a skin cancer diagnosis tool that classifies different skin lesions as
+            specific types of malignant and benign cancers (i.e. melanoma, moles, warts, etc.). 
+            Specifically, we will be classifying between actinic keratoses and intraepithelial 
+            carcinoma / Bowen's disease, basal cell carcinoma, benign keratosis-like lesions 
+            (noncancerous UV damage), dermatofibroma, melanoma, melanocytic nevi, and vascular 
+            lesions such as angiomas, angiokeratomas, pyogenic granulomas and hemorrhage. 
+          </p>
+          <p>
+            We created this tool through the use of various convolutional neural networks. 
+            Our results from a variety of convolutional neural network models were analyzed 
+            and compared in terms of accuracy and efficiency. Each neural network took in 
+            a photo as input and the models outputted a classification from those 
+            aforementioned inputs.  
+          </p>
+          <p>
+            This is an interesting problem because skin cancer is one of the most prevalent types 
+            of cancer. According to the World Health Organization, one in every three cancers 
+            diagnosed each year is skin cancer and one in five people are diagnosed throughout their 
+            life.  As such, skin cancer is a deadly disease which impacts many people. Lesions are 
+            easy to ignore as they seem trivial; having such a classification tool could drastically 
+            assist in catching skin cancers early and thus improve survival rates. The goal of this 
+            model is to classify various lesions as a means of helping a user to determine whether 
+            any given lesion is malignant or benign.  
+          </p>
+        </div>
+
+      </div>
+
       
 
     </div>
