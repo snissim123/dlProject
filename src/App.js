666dc83ae22141ac480d5abceaae9a9e209fce15
Sandra Nissim <sandranissim@Sandras-MBP.hsd1.ca.comcast.net>
2020-12-01 18:35:35 -0800

    initial page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const headerStyle = {
+  backgroundColor: "#416975",
+  padding: 40
+};
+
+const titleStyle = {
+  color: "#ADCEDE",
+  fontSize: "32px",
+}
+
+const subStyle = {
+  color: "#1B2C36",
+  fontSize: "18px",
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header style={headerStyle}>
+          <p style={titleStyle}>Skin Lesion Classification</p>
       </header>
+      <body style={subStyle}>
+        <p>Jordan Zax and Sandra Nissim</p>
+        <p>jordanzax2021@u.northwestern.edu sandranissim2021@u.northwestern.edu</p>
+        <p>Northwestern University</p>
+        <p>Computer Science 396/496: Deep Learning</p>
+        <p>Professor Bryan Pardo</p>
+      </body>
+      
+
     </div>
   );
 }
+
 
 export default App;
